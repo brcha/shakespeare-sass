9e07c2c14a6f0201e7804968a0a29fa58732ad7a
Filip Brcic <brcha@gna.org>
2016-05-05 00:28:55 +0200

    Scrapped SASS.hs from test-project snippets (initial commit of SASS.hs)

---
+++ src/Text/Shakespeare/SASS.hs
@@ -0,0 +1,47 @@
+-- ----- BEGIN LICENSE BLOCK -----
+-- Version: MPL 2.0
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- ----- END LICENSE BLOCK -----
+
+--
+-- Copyright (c) 2016, Filip Brcic <brcha@gna.org>. All rights reserved.
+--
+-- This file is part of shakespeare-sass
+--
+module Text.Shakespeare.SASS
+    ( wsass'
+    , wfsSASS
+    ) where
+
+import Text.Sass
+import Language.Haskell.TH
+import qualified Data.Default as DD
+import Yesod.Default.Util (WidgetFileSettings, TemplateLanguage (..)
+                          ,defaultTemplateLanguages, wfsLanguages)
+import Yesod.Core
+
+wsass' :: [FilePath] -> FilePath -> Q Exp
+wsass' incPath fileName = do
+    css <- runIO $ compileSassFile (Just incPath) fileName
+    [| CssBuilder $(stringE css) |]
+
+compileSassFile :: Maybe [FilePath] -> FilePath -> IO String
+compileSassFile incPath fileName = do
+    result <- compileFile fileName ( DD.def { sassIncludePaths = incPath } )
+    case result of
+        Left err -> do
+            err' <- errorMessage err
+            error err'
+        Right compiled -> return . resultString $ compiled
+
+wfsSASS :: [FilePath] -> WidgetFileSettings
+wfsSASS sassInclude = def { wfsLanguages = \hset -> defaultTemplateLanguages hset ++
+    [ TemplateLanguage True  "sass" wsass wsass
+    , TemplateLanguage True  "scss" wsass wsass
+    ] }
+    where
+         wsass = wsass' sassInclude
